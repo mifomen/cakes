b503a7e023b0cca4e05a655a3fbe7eea2467aa67
Ilya Koshelev <mifomen@yandex.ru>
2018-04-16 07:56:10 +0300

    initiate project

---
+++ gulp/less.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cfg = require('../package.json').config;
+
+var plumber = require('gulp-plumber');
+var rename = require("gulp-rename");
+var csso = require('gulp-csso');
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer"); 
+
+var browserSync = require('browser-sync').create(); 
+var csso = require('gulp-csso'); 
+var mqpacker = require("css-mqpacker"); 
+var minify = require("gulp-csso"); 
+// var del = require("del");
+
+gulp.task('less', function() {
+  return gulp.src( cfg.src.less + '/event.less')
+  .pipe(plumber())
+  .pipe(less())
+  .pipe(postcss([ 
+    autoprefixer({ browsers: [
+  'last 10 versions', 
+  'ie 11',
+  'ie 10',
+  'Android >= 4.1', 
+  'Safari >= 8',
+  'iOS >= 8'
+  ] }),     
+  mqpacker({ sort: true })
+]))
+  .pipe(csso())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest(cfg.build.css))
+  .pipe(browserSync.stream());
+});
